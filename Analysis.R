85c352b91604c19f3fff5f5e05076be8ec3ec3bc
Seina Yamada <97848654+pp203@users.noreply.github.com>
2022-01-22 17:41:14 -0500

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -1,10 +1,7 @@
-#HW 4
-#4DO3
-#Seina Yamada (400088461)
 library(dplyr)
 library(stringr)
 #-----------------------------------------------------------------------------------------------------------------------
-#DATA
+#Data Analysis
 
 #1
 Meta <- read.csv("FakeNewsMeta.csv", stringsAsFactors = FALSE)
@@ -20,17 +17,6 @@ A1 <- Meta %>% group_by(type) %>% summarise(AverageShare = mean(shares)) %>% arr
 #4
 A2 <- Meta %>% count(author) %>% arrange(desc(n))
 head(A2, 10)
-#1         unknown 
-#2           admin  
-#3     Alex Ansary 
-#4     Eddy Lavine  
-#5          Editor
-#6         Gillian  
-#7        Pakalert
-#8        Starkman  
-#9  BareNakedIslam
-#10    Dave Hodges 
-#NOTE: between #3 and #8 are the same number.
 
 #5
 # Meta %>% filter(!str_detect(author, "^$")) 
@@ -46,37 +32,34 @@ PPD %>% filter(author == "Eddy Lavine") %>% arrange(Date)
 Meta %>% filter(author == "Eddy Lavine") %>% count(country)
 #Eddy Lavine was posting from TV 
 
-#8
-#When I search the relationship between Nov 2nd and Tuvalu,
-#I found that climate change in the country, which co-operated with EU, ESCAP, ILO and UNDP.
 #-----------------------------------------------------------------------------------------------------------------------
-#TEXT
+#Text Analysis
 
 #9
 Titles <- readLines("FakeNewsTitles.txt", encoding = "UTF-8")
 View(Titles)
 
 #10
-#a
+#10-1
 Titles <- str_remove_all(Titles, pattern = "(\\||»).*$")
 
-#b
+#10-2
 Titles <- str_remove_all(Titles, pattern = "(\\[|\\()[a-zA-Z\\s]*(\\]|\\))")
 
-#c
+#10-3
 Titles <- str_remove_all(Titles, pattern = "\\s[a-zA-Z0-9]*\\.com$")
 
 #11
-#a
+#11-1
 Titles <- str_to_lower(Titles)
 
-#b
+#11-2
 Titles <- str_replace_all(Titles, "\u2026", " ")
 
-#c
+#11-3
 Titles <- str_remove_all(Titles, pattern = "[:punct:]")
 
-#d
+#11-4
 Titles <- str_trim(Titles, side = "both")
 Titles <- str_squish(Titles)
 
